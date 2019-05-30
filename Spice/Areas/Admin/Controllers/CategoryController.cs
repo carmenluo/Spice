a3fea212a394f8e9c3cbe8d0226f3142528f1c65
Carmen Luo <sdluojm@gmail.com>
2019-05-30 14:41:22 -0400

    Fix "delete" in Index directly

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -20,6 +21,72 @@ namespace Spice.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _db.Category.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(category);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+
+        }
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(category);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var deleteItem =await _db.Category.FindAsync(id);
+            if (deleteItem == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Category.Remove(deleteItem);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
         }
     }
 }
