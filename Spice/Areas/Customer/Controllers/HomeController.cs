692ea8013d6d254f877cb130f47586cc106d839d
Carmen Luo <sdluojm@gmail.com>
2019-07-16 16:12:51 -0400

    Add files via upload

---
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models;
+using Spice.Models.ViewModel;
+
+namespace Spice.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public HomeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel IndexVM = new IndexViewModel()
+            {
+                MenuItems = await _db.Menu.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync(),
+                CategoryItems = await _db.Category.ToListAsync(),
+                CouponItems = await _db.Coupon.Where(m=>m.isActive==true).ToListAsync()
+
+            };
+            return View(IndexVM);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
